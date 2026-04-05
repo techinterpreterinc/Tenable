f7d2a6c7b2f71fa7d4575029e38d76427c84be0d
Technology Interpreters <kendrick@technologyinterpreters.com>
2026-04-05 00:08:27 -0400

    Update Install Tenable Nessus Agent macOS.sh

--- Install Tenable Nessus Agent macOS.sh
+++ Install Tenable Nessus Agent macOS.sh
@@ -1,1 +1,5 @@
-
+curl --request GET \
+  --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/NessusAgent-11.1.2.dmg' \
+  --output '/tmp/NessusAgent-11.1.2.dmg'
+hdiutil attach /tmp/NessusAgent-11.1.2.dmg
+sudo installer -pkg /Volumes/Nessus\ Agent\ Install/Install Nessus Agent.pkg -target /
